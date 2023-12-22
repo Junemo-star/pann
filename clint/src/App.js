b6589d3861aa83c0eb485fb1f259a5ab62cf3578
ศรัณยพงศ์ เอี่ยมอนงค์ <saranyapong272547@gmail.com>
2023-12-22 22:40:20 +0700

    add login page

---
+++ clint/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Login from './component/Login';
+
+function App() {
+  //const clickme = () => console.log('Hello world')
+
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ clint/src/component/Studentpage.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+function StudentPage() {
+
+    return (
+      <div class="container">
+        Student Page
+      </div>
+    );
+  }
+  
+  export default StudentPage;
+  
+  
